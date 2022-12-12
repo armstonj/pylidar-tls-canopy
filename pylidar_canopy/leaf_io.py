fixed errors in calculation of corrected zenith and azimuth angles

pylidar_canopy/leaf_io.py
@@ -33,9 +33,9 @@ class LeafScanFile:
             self.azimuth_shots = int(fileinfo[7])
         else:
             print(f'{filename} is not a recognized LEAF scan file')
+        self.read_meta()
 
     def __enter__(self):
-        self.read_meta()
         self.read_data()
         return self
 
@@ -88,7 +88,7 @@ class LeafScanFile:
         self.data['datetime'] = [self.datetime + timedelta(milliseconds=s)
             for s in self.data['sample_time'].cumsum()]
 
-        self.data['zenith'] = (self.data['scan_encoder'] / 1e4 * 2 * np.pi) - np.pi
+        self.data['zenith'] = self.data['scan_encoder'] / 1e4 * 2 * np.pi
         self.data['azimuth'] = self.data['rotary_encoder'] / 2e4 * 2 * np.pi
 
         if transform:
@@ -97,6 +97,12 @@ class LeafScanFile:
             self.data['zenith'] += theta
             self.data['azimuth'] += phi
 
+        self.data['zenith'] = np.abs(self.data['zenith'] - np.pi)
+        idx = self.data['azimuth'] > (2 * np.pi)
+        self.data.loc[idx,'azimuth'] = self.data.loc[idx,'azimuth'] - (2 * np.pi)
+        idx = self.data['azimuth'] < 0
+        self.data.loc[idx,'azimuth'] = self.data.loc[idx,'azimuth'] + (2 * np.pi)
+ 
         for i,name in enumerate(['range1','range2']):
             n = i + 1
             x,y,z = rza2xyz(self.data[name], self.data['zenith'], self.data['azimuth'])

pylidar_canopy/plant_profile.py
@@ -65,9 +65,10 @@ class Jupp2009:
             sum_by_index_3d(w, z_idx, a_idx, h_idx, self.target_output)
         elif method == 'FIRST':
             idx = (target_index == 1)
-            w = np.ones(np.count_nonzero(idx), dtype=np.float32)
-            sum_by_index_3d(w, z_idx[idx], a_idx[idx], h_idx[idx], 
-                self.target_output)
+            if np.any(idx):
+                w = np.ones(np.count_nonzero(idx), dtype=np.float32)
+                sum_by_index_3d(w, z_idx[idx], a_idx[idx], h_idx[idx], 
+                    self.target_output)
         else:
             print(f'{method} is not a recognized target weighting method')
             sys.exit()
@@ -184,6 +185,7 @@ class Jupp2009:
         max_zenith_r = np.radians(max_zenith)
 
         with leaf_io.LeafScanFile(leaf_file, sensor_height=sensor_height) as leaf:
+            self.datetime = leaf.datetime
             if not leaf.data.empty:
                 # Point data
                 azimuth = leaf.data['azimuth'].to_numpy()
@@ -259,7 +261,13 @@ class Jupp2009:
         """
         zenith_bin_r = np.radians(self.zenith_bin)
         hingeindex = np.argmin(np.abs(zenith_bin_r - np.arctan(np.pi / 2)))
-        pai = -1.1 * np.log(self.pgap_theta_z[hingeindex,:])
+        
+        pai_lim = np.log(1e-5)
+        tmp = np.full(self.pgap_theta_z.shape[1], pai_lim)
+        np.log(self.pgap_theta_z[hingeindex,:], out=tmp, 
+            where=self.pgap_theta_z[hingeindex,:] > 0)
+        
+        pai = -1.1 * tmp
 
         return pai
 