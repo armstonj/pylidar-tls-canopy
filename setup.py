ba07e7f10b1fd4268282fa3d04028d0014ae87a0
armstonj <armston@umd.edu>
2023-03-12 17:13:53 -0400

    minor change to installation

--- setup.py
+++ setup.py
@@ -87,7 +87,6 @@ setup(name='pylidar-tls-canopy',
       description='Tools for canopy gap probability modeling using RIEGL VZ and LEAF TLS measurements',
       classifiers=['Intended Audience :: Developers',
           'Operating System :: OS Independent',
-          'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.9',
           'Programming Language :: Python :: 3.10'
